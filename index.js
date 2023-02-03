5a9add1637f1ea8f9c2a62dba3700e0e69288ed1
Tarrol <quickandrey@gmail.com>
2023-02-03 09:27:25 -0800

    Simple input/output

--- index.js
+++ index.js
@@ -4,5 +4,20 @@ const fs = require('fs');
 
 function App() {
   console.log("Hello world.");
+  // Start building the team using 
+  function createTeamLead() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'teamLeadName',
+        message: 'What is the name of the of the team lead?'
+      }
+    ])
+      .then((answer) => {
+        console.log(answer.teamLeadName);
+      })
+  }
+  createTeamLead();
 }
-App()
+
+App();
